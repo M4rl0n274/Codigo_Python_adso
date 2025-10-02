3eda501167de304f471ad4bebdbdc8afa2313381
M4rl0n274 <marlonquinteri274@gmail.com>
2025-10-01 22:50:04 -0500

    carpetas actividad resolución de problemas algorítmicos

---
+++ 2.Resolución_problemas_algorítmicos_GA3-220501093-AA3-EV02/problema2.py
@@ -0,0 +1,7 @@
+# Escriba un programa que lea dos vectores de números enteros ordenados ascendentemente y
+# luego produzca la lista ordenada de la mezcla de los dos, por ejemplo, si los dos arreglos tienen los
+# números 1 3 6 9 17 y 2 4 10 17, respectivamente, la lista de números en la pantalla debe ser 1 2 3 4
+# 6 9 10 17 17. Limite los vectores a un tamaño de 5 y debe validar en cada ingreso que realmente se
+# estén ingresando los datos de forma ascendente.
+
+
---
+++ 2.Resolución_problemas_algorítmicos_GA3-220501093-AA3-EV02/problema3.py
@@ -0,0 +1,16 @@
+# Una emisora con presencia en diferentes ciudades, desea conocer el rating de canciones y
+# cantantes más escuchados (sonados) en este semestre del año. Por lo tanto, se ha pedido a
+# aprendices del SENA desarrollar una solución que permita conocer la respuesta de 6 personas con
+# relación a sus gustos musicales. Con fines administrativos y realizar una rifa entre las personas
+# encuestadas, la emisora desea poder registrar de las personas entrevistadas su nombre, número de
+# identificación (cédula), fecha de nacimiento, correo electrónico, ciudad de residencia, ciudad de
+# origen. Además, se deberá poder almacenar el artista y título de hasta 3 canciones favoritas en
+# cada una de las personas que se ingrese. Teniendo en cuenta lo anterior, se sugiere que la solución
+# deberá mostrar un menú que permita las siguientes opciones:
+
+# a. Agregar una persona con los datos que se listan anteriormente.
+# b. Mostrar la información personal de una persona particular por medio de su posición en el
+# vector.
+
+
+
